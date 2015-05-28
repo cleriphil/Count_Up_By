e35567e98699e838c5f05590b71b2dd7efd91443
Frank Johnson & cleri philiastides <student@epicodus.com>
2015-05-28 10:01:15 -0700

    add front end to display a form and results

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,19 @@ var countUpBy = function(start, end, interval) {
   }
   return array;
 };
+
+
+$(function() {
+  $('form#numbers').submit(function(event) {
+    $('#result').show();
+    var start = parseInt(($('input#start')).val());
+    var end = parseInt(($('input#end')).val());
+    var interval = parseInt(($('input#interval')).val());
+
+    var output = countUpBy(start, end, interval);
+
+    $(".result").text(output);
+
+    event.preventDefault();
+  });
+});
