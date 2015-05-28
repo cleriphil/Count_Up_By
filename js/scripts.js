7548ba11f2bea157c19ce4872f4c23bcf3e8e535
Frank Johnson & cleri philiastides <student@epicodus.com>
2015-05-28 10:06:13 -0700

    Add error message for when interval is less than the end number

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,14 @@
 var countUpBy = function(start, end, interval) {
   var array = [];
-  for (var i = start; i <= end; i += interval) {
-    array.push(i);
-  }
-  return array;
+
+  if (interval < end) {
+    for (var i = start; i <= end; i += interval) {
+      array.push(i);
+    }
+    return array;
+  } else {
+    alert("Interval must be less than the end number.")
+  };
 };
 
 
@@ -15,6 +20,7 @@ $(function() {
     var interval = parseInt(($('input#interval')).val());
 
     var output = countUpBy(start, end, interval);
+    output = output.join(', ');
 
     $(".result").text(output);
 
